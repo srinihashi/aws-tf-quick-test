b11b0aecb4beb0f33233cb41c3ca8631954e4744
Srini Nagaraju <37597474+srinihashi@users.noreply.github.com>
2022-10-13 12:42:48 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.global_region
+
+  default_tags {
+    tags = {
+        Env = var.global_tag_env
+        Owner = var.global_tag_owner
+        tf_code = var.global_tag_tf_code
+    }
+  }
+}
