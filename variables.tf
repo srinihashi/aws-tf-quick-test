51a2c9a2a5f1132ed0106316df16dd389c2e58cd
Srini Nagaraju <37597474+srinihashi@users.noreply.github.com>
2022-10-13 12:34:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,45 +1,54 @@
 variable "global_region" {
   type = string
   description = "Region to deploy resources"
+  default = "us-west-2"
 }
 
 variable "global_tag_prefix" {
   type = string
   description = "tags_all: Prefix"
+  default = "sri"
 }
 
 variable "global_tag_owner" {
   type = string
   description = "tags_all: Owner"
+  default = "Srini"
 }
 
 variable "global_tag_env" {
   type = string
   description = "tags_all: env"
+  default = "dev"
 }
 
 variable "global_tag_tf_code" {
   type = string
   description = "tags_all: Path to Terraform config"
+  default = "TFC/srini-hashi-aws/test"
 }
 
 variable "subnet-id" {
   type = string
   description = "Subnet to deploy the ec2 instnace"
+  default = "subnet-0e003577a33fc632a"
 }
 variable "instance-type" {
   type = string
   description = "Type of AWS ec2 instance type (Ex: t2.micro, t2.small)"
+  default = "t3.micro"
 }
 
 variable "aws-ami" {
   type = string
   description = "AWS AMI id (Ex: ami-098e42ae54c764c35)"
+  default = "ami-098e42ae54c764c35"
 }
 
 variable "associate-public-ip-address" {
   type = bool
   description = "Set to true if the ec2 instance needs to be assigned public IP"
+  default = true
 }
 
 variable "key-name" {
@@ -54,4 +63,5 @@ variable "vpc-security-group-ids" {
 variable "ec2-name" {
   type = string
   description = "Name of AWS ec2 instance"
+  default = "Testing"
 }
